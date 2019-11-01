3796cf299b7fa4fc53931026540f9ffe61e388f5
aizwellenstan <aizwellenstan@gmail.com>
2019-11-01 18:40:03 +0900

    add functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
+
     def create
         user = User
                 .find_by(email: params["user"]["password"])
@@ -11,7 +13,25 @@ class SessionsController < ApplicationController
                 user: user
             }
         else
-            render json { status: 401}
+            render json { status: 401 }
+        end 
+    end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
         end
+    end
+    
+    def logout
+        reset_session
+        render json: { status: 200, logged_out: true }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :loggen_in, to: "sessions#loggen_in"
   root to: "static#home"
 end
